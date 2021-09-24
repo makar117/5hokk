e73497fe4c9079d2507e9ad1b3b9ee5c7af54344
5hokk <makarkolpakov1@gmail.com>
2021-09-24 09:38:04 +0300

    .

---
+++ bebra.py
@@ -0,0 +1,34 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+					level=logging.INFO,
+					filename='bot.log'
+					)
+
+def start_bot(update: Updater, context: CallbackContext):
+	print(update) 
+	mytext = """Аллейкум Асалам {}
+
+	Аллаху Акбар""".format(update.message.chat.first_name)
+	update.message.reply_text(mytext)
+
+def chat(update: Updater, context: CallbackContext):
+	text= update.message.text
+	logging.info(text) 
+
+	update.message.reply_text(text)	
+
+def main():
+	updtr = Updater(settings.TOKEN_TELEGRAM)
+
+	updtr.dispatcher.add_handler(CommandHandler("salam", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+	updtr.start_polling()
+	updtr.idle()
+
+if __name__ == "__main__":
+	logging.info('Bot started')
+	main()	
